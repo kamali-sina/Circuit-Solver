f95bca0ce2aa37edd7064f70a14d09b00d1f1f74
Sina Kamali <Sina Kamali>
2020-07-20 20:50:28 +0430

    added wire and made tweaks

--- classes.py
+++ classes.py
@@ -17,25 +17,36 @@ class Inductor :
         self.value = value
 
 class DependentCurrentSource :
-    def __init__(self, beginning, end, value) :
+    def __init__(self, beginning, end,source,bias, value) :
         self.beginning_position = beginning
         self.end_position = end
         self.value = value
+        self.bias = bias
+        self.source = source
 
 class IndependentCurrentSource :
-    def __init__(self, beginning, end, value) :
+    def __init__(self, beginning, end,source,bias, value) :
         self.beginning_position = beginning
         self.end_position = end
         self.value = value
 
+
 class DependentVoltageSource :
-    def __init__(self, beginning, end, value) :
+    def __init__(self, beginning,source,bias, end, value) :
         self.beginning_position = beginning
         self.end_position = end
         self.value = value
+        self.bias = bias
+        self.source = source
 
 class IndependentVoltagetSource :
     def __init__(self, beginning, end, value) :
         self.beginning_position = beginning
         self.end_position = end
         self.value = value
+        
+
+class Wire :
+    def __init__(self, beginning, end) :
+        self.beginning_position = beginning
+        self.end_position = end
